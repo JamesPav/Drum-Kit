7243de659bb14297e6b4b3e4026bc513b2e5ec27
Jim Pav <jamesvlid@hotmail.com>
2020-08-03 06:03:25 +0300

    added click function

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //event listener
 window.addEventListener("keydown", playSound);
+window.addEventListener("click", clickSound);
 
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -17,4 +18,24 @@ keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
   this.classList.remove("playing");
+}
+
+function clickSound(e) {
+  if (
+    e.target.classList.contains("key") ||
+    e.target.classList.contains("sound") ||
+    e.target.tagName == "KBD"
+  ) {
+    const audio = document.querySelector(
+      `audio[data-key="${e.target.getAttribute("data-key")}"]`
+    );
+    const key = document.querySelector(
+      `.key[data-key="${e.target.getAttribute("data-key")}"]`
+    );
+
+    if (!audio) return; // return nothing (exit) if there is no key assigned
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+  }
 }
