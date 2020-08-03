68d7f827bec6f63a223c83e8f6b604984315a778
Jim Pav <jamesvlid@hotmail.com>
2020-08-03 23:41:48 +0300

    made some changes to style to be responsive

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ window.addEventListener("click", clickSound);
 
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const box = document.querySelector(`.box[data-key="${e.keyCode}"]`);
 
   if (!audio) return; // return nothing (exit) if there is no key assigned
-  key.classList.add("playing");
+  box.classList.add("playing");
   audio.currentTime = 0;
   audio.play();
 }
 
-const keys = document.querySelectorAll(".key");
-keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+const keys = document.querySelectorAll(".box");
+keys.forEach((box) => box.addEventListener("transitionend", removeTransition));
 
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
@@ -22,19 +22,19 @@ function removeTransition(e) {
 
 function clickSound(e) {
   if (
-    e.target.classList.contains("key") ||
+    e.target.classList.contains("box") ||
     e.target.classList.contains("sound") ||
     e.target.tagName == "KBD"
   ) {
     const audio = document.querySelector(
       `audio[data-key="${e.target.getAttribute("data-key")}"]`
     );
-    const key = document.querySelector(
-      `.key[data-key="${e.target.getAttribute("data-key")}"]`
+    const box = document.querySelector(
+      `.box[data-key="${e.target.getAttribute("data-key")}"]`
     );
 
     if (!audio) return; // return nothing (exit) if there is no key assigned
-    key.classList.add("playing");
+    box.classList.add("playing");
     audio.currentTime = 0;
     audio.play();
   }
